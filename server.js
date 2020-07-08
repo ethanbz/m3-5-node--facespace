a01732400bc4407356da79fb6ea6e6fcd2ebc9f3
ethanbz <ethanzhivotovsky@gmail.com>
2020-07-08 15:49:43 -0400

    workshop3.5Final

--- server.js
+++ server.js
@@ -12,6 +12,39 @@ const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
 
+const handleHomepage = (req, res) => {
+  res.status(200).render('pages/homepage', { users, currentUser });
+}
+
+const handleUserPage = (req, res) => {
+  let page = {};
+  users.forEach(user => {
+    if (user._id === req.params.id) {page = user;};
+  });
+  res.status(200).render('pages/profile', { user: page, users, currentUser });
+}
+
+const handleSignin = (req, res) => {
+  if (currentUser.name) {
+    res.status(200).render('pages/profile', { user: currentUser, users, currentUser });
+  } else {
+    res.status(200).render('pages/signin', { currentUser });
+  }
+}
+
+const handleName = (req, res) => {
+  let firstName = req.body.firstName.toLowerCase();
+  let redirect = false;
+  users.forEach(user => {
+    if (user.name.toLowerCase() === firstName) {
+      currentUser = user;
+      res.status(200).render('pages/profile', { user, users, currentUser });
+      redirect = true;
+    }
+  })
+  if (!redirect) res.status(404).redirect('/signin')
+}
+
 // -----------------------------------------------------
 // server endpoints
 express()
@@ -21,7 +54,10 @@ express()
   .set('view engine', 'ejs')
 
   // endpoints
-
+  .get('/', handleHomepage)
+  .get('/users/:id', handleUserPage)
+  .get('/signin', handleSignin)
+  .post('/getname', handleName)
   // a catchall endpoint that will send the 404 message.
   .get('*', handleFourOhFour)
 
